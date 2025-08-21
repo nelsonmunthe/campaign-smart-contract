2581ff58f45aeacc9c89888474b022eaef63aff2
Nelson Munthe <nnelson.munthe@gmail.com>
2025-08-21 08:40:11 +0800

    first commit smart contract

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -3,7 +3,21 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Campaign {
-    constructor() {
-        
+
+    struct Request {
+        string description;
+        uint value;
+        address recepient;
+        bool complete;
+    }
+
+    address public manager;
+    uint public minimumContribution;
+    address[] public approvers;
+    Request[] public requests;
+
+    constructor(uint minimum) {
+        minimumContribution = minimum;
+        manager = msg.sender;
     }
 }
